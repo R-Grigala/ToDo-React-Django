4c26fbf4cd81d78e265534f17503c49e26ccfc08
R-Grigala <r.grigalashvili777@gmail.com>
2022-11-15 00:08:29 +0400

    Coding continue

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
 
-const Todo = () => {
-  return (
-    <div>Todo</div>
-  )
+import done_icon from '../assets/images/done_icon.svg'
+import delete_icon from '../assets/images/delete_icon.svg'
+
+const Todo = ({todos, completeTodo, removeTodo}) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ''
+  })
+
+  return todos.map((todo, index) => (
+    <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className='icons'>
+        <div className='done_icon' onClick={() => removeTodo(todo.id)}>
+          <img src={done_icon} alt='done_icon'/>
+        </div>
+        <div className='delete_icon'>
+          <img src={delete_icon} alt='delete_icon'/>
+        </div>
+      </div>
+    </div>
+  ))
 }
 
 export default Todo
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import TodoForm from './TodoForm'
 
 const TodoList = () => {
@@ -12,13 +13,34 @@ const TodoList = () => {
     const newTodos = [todo, ...todos]
 
     setTodos(newTodos);
-    console.log(...todos);
-  }
+    console.log(todo, ...todos);
+  };
+
+  const deleteTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id)
+
+    setTodos(removeArr);
+  };
+
+  const completeTodo = id => {
+    let updateTodos = todos.map(todo => {
+      if(todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updateTodos);
+  } 
 
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
       <TodoForm onSubmit={addTodo}/>
+      <Todo 
+      todos={todos}
+      completeTodo={completeTodo}
+      removeTodo={deleteTodo}
+      />
     </div>
   )
 }
