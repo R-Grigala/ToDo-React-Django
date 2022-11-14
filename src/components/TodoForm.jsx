f0b6fc807be43f36936c07ba43e0f9e13175885b
R-Grigala <r.grigalashvili777@gmail.com>
2022-11-14 22:56:42 +0400

    Coding Continue

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -10,16 +10,16 @@ const TodoForm = (props) => {
     const handleSubmit = e => {
         e.preventDefault();
 
-        // props.onSubmit({
-        //     id: Math.floor(Math.random() * 10000),
-        //     text: input
-        // });
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        });
 
         setInput('');
     }
 
     return (
-        <form className='todo-form'>
+        <form className='todo-form' onSubmit={handleSubmit}>
             <input
                 type='text' 
                 placeholder='Add a todo' 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,13 +5,20 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
 
   const addTodo = todo => {
-    
+    if(!todo.text || /^\s*$/.test(todo.text)){
+      return
+    }
+
+    const newTodos = [todo, ...todos]
+
+    setTodos(newTodos);
+    console.log(...todos);
   }
 
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo}/>
     </div>
   )
 }
