1272eec539c31d674fec65f3d3a31f1e3c1be839
R-Grigala <r.grigalashvili777@gmail.com>
2022-11-24 01:38:08 +0400

    Coding Jakhiring

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -3,6 +3,7 @@ import ROUTES from "../config/ROUTES";
 import ToDo from "../img/ToDo.png"
 import { useEffect } from "react";
 import defaultsToLocalStorage from "../functions/defaultsToLocalStorage";
+import '../styles/GetStarted.css'
 
 function GetStarted() {
     useEffect(() => {
@@ -12,11 +13,11 @@ function GetStarted() {
     }, [])
 
     return (
-        <section className="bg-black text-center h-screen px-8">
-            <img alt="ToDo" src={ToDo} className="pt-10 mx-auto w-[90.36px] pt-16" />
-            <h1 className='text-white text-3xl sm:text-4xl md1000:text-[54px] font-semibold mt-11 font-main'>Keep Track Of Daily Tasks In Life</h1>
+        <section className="section">
+            <img alt="ToDo" src={ToDo} className="ToDo-img" />
+            <h1 className='text-getstarted'>Keep Track Of Daily Tasks In Life</h1>
             <Link to={ROUTES.SIGNIN}>
-                <button className='bg-main-green font-light text-[28px] md1000:text-[48px] mt-24 sm:mt-40 px-10 py-4 rounded'>Get Started</button>
+                <button className='btn-getstarted'>Get Started</button>
             </Link>
         </section >
     );
