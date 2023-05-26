182afa5b392c9ea1fa30fd212d5897ea90b87d3c
R-Grigala <r.grigalashvili777@gmail.com>
2023-05-27 00:47:24 +0400

    Coding Continue:// Add TaskDelete and TaskUpdate

--- backend/tasks_api/urls.py
+++ backend/tasks_api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', ApiOverview.as_view(), name="api_overview"),
     path('task-list/', TaskList.as_view(), name="task_list"),
     path('task-detail/<str:pk>/', TaskDetail.as_view(), name="task_detail"),
-    path('task-create/', TaskCreate.as_view(), name="task_create")
+    path('task-create/', TaskCreate.as_view(), name="task_create"),
+    path('task-update/<str:pk>/', TaskUpdate.as_view(), name="task_update"),
+    path('task-delete/<str:pk>/', TaskDelete.as_view(), name="task_delete"),
 ]
--- backend/tasks_api/views.py
+++ backend/tasks_api/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework import status
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -15,7 +14,6 @@ class ApiOverview(APIView):
             'Update': '/task-update/<str:pk>/',
             'Delete': '/task-delete/<str:pk>/',
         }
-
         return Response(api_urls)
     
 class TaskList(APIView):
@@ -23,19 +21,54 @@ class TaskList(APIView):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
-    
+
 class TaskDetail(APIView):
     def get(self, request, pk):
-        task = Task.objects.get(task_id=pk)
-        serializer = TaskSerializer(task, many=False)
-        return Response(serializer.data)
-    
+        try:
+            task = Task.objects.get(task_id=pk)
+            serializer = TaskSerializer(task, many=False)
+            return Response(serializer.data)
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 class TaskCreate(APIView):
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
+        try:
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return Response(serializer.data)
-    
+class TaskUpdate(APIView):
+    def post(self, request, pk):
+        try:
+            task = Task.objects.get(task_id=pk)
+            serializer = TaskSerializer(instance=task, data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class TaskDelete(APIView):
+    def delete(self, request, pk):
+        try:
+            task = Task.objects.get(task_id=pk)
+            task.delete()
+            return Response('Item succsesfully delete!')
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
